b8248fb1a6a9c17ba44b4d677633f7fbc7a59d2a
Earl006 <earljoe06@gmail.com>
2024-06-05 10:17:45 +0000

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Function  to query number of subscribers given the reddit subreddit"""
 import requests
 
-def number of subscribers(subreddit):
-	url="https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {
-		"User Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-	}
-	response = requests.get(url, headers=headers, allow_redirects=False)
-	if(response.status_code == 404
-		return 0
-	results = response.json().get(data)
-	return results.get("subscribers")
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)" 
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200: 
+        return 0
+    results = response.json().get("data", {})
+    return results.get("subscribers")
