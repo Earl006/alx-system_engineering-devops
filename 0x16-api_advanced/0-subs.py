3022d079ff776c3f4a2097073e7b111344ef7cea
Earl006 <earljoe06@gmail.com>
2024-06-05 02:23:05 -0700

    0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,9 +15,9 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
+    response = get(url)
     results = response.json()
 
     try:
